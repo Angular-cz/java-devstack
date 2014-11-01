e0fa8218960cf2df3253d8d6fa08efe9aec82e5e
Víťa Plšek <vita@angular.cz>
2014-11-01 16:43:23 +0100

    smazání generovaných scriptů

--- java-devstack-frontend/src/main/frontend/src/templates.js
+++
@@ -1,1 +0,0 @@
-angular.module("javaDemo").run(["$templateCache", function($templateCache) {$templateCache.put("user/detail/detail.html","<h1>User detail</h1>\n\n<div class=\"panel\">Hello {{ctrl.user.name}}</div>\n\n<pre>\n{{ctrl.user | json}}\n</pre>");}]);
